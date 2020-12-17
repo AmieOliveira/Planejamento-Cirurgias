bd7d53dd8ab378b05dd50ac7b5edd55b5c12f243
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2020-12-16 22:10:35 -0200

    Detalhes dos resultados

--- Code/main.jl
+++ Code/main.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames
+using CSV, DataFrames, Printf
 using Debugger
 
 include("helper.jl")
@@ -14,11 +14,16 @@ function load_surgeries(filepath)
     surgeries
 end
 
+# Paths
+data_dir = "Dados/"
+out_dir = "Soluções/"
+
 # load instance parameters
+data_root = "toy2"    # "fullrand_s20_p1-4_w0-15_t4-16_e5_g8"
+rooms = 2
 days = 5
 penalties = [900, 200, 50, 10] #TODO: these priorities were tweaked. should confirm
-surgeries = load_surgeries("Dados/toy1.csv")    # "Dados/fullrand_s20_p1-4_w0-15_t4-16_e5_g8.csv") # ("../Dados/toy1.csv")
-rooms = 1
+surgeries = load_surgeries(@sprintf("%s%s.csv", data_dir, data_root)) 
 
 # Solution set up
 instance = (surgeries, rooms, days, penalties)
@@ -27,9 +32,12 @@ solution = solve(instance, verbose=false)
 # TODO: criar dentro dos solvers - room_specialties = zeros(Int, (rooms, days))        # Vetor com as especialidades das salas pelos dias
 
 print_solution(instance, solution)
-fn = target_fn(instance, solution);#, true)
 println("")
+println("Scheduling costs: ")
+fn = target_fn(instance, solution, true)
 println("Target function: ", fn)
+bad = badly_scheduled(surgeries, solution)
+plot_solution(instance, solution, @sprintf("%s%s-greedy", out_dir, data_root))
 
 #solution = random_removal(instance, solution)
 #solution = random_removal(instance, solution)
@@ -43,6 +51,9 @@ solution = Debugger.@run alns_solve(instance, solution, SA_max=10, α=0.9, T0=60
 sc_d, sc_r, sc_h = solution
 println("ALNS solution:", solution)
 print_solution(instance, solution)
-fn = target_fn(instance, (sc_d, sc_r, sc_h));#, true)
 println("")
+println("Scheduling costs: ")
+fn = target_fn(instance, (sc_d, sc_r, sc_h), true)
 println("Target function: ", fn)
+bad = badly_scheduled(surgeries, solution)
+plot_solution(instance, solution, @sprintf("%s%s-alns", out_dir, data_root))
