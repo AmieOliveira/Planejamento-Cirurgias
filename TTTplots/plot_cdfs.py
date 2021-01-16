1d69a9363c493e8ffe0c3997b4fef77e13743b69
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2021-01-16 13:41:13 -0200

    tttplots i10 e i13

--- TTTplots/plot_cdfs.py
+++ TTTplots/plot_cdfs.py
@@ -1,30 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-titulo = "I11\nσ1=10, σ2=5, σ3=15. Objetivo: 139500"         # I5
+titulo = "I10\nσ1=10, σ2=5, σ3=15. Objetivo: 98400"         # I10
 # "I1\nσ1=10, σ2=5, σ3=15. Objetivo: 260870"        # I1
 # "I2\nσ1=10, σ2=5, σ3=15. Objetivo: 42778"         # I2
 # "I2\nσ1=10, σ2=5, σ3=15. Objetivo: 383300"        # I3
 # "I4\nσ1=10, σ2=5, σ3=15. Objetivo: 1497200"       # I4
 # "I5\nσ1=10, σ2=5, σ3=15. Objetivo: 1526900"       # I5
-#
+# "I8\nσ1=10, σ2=5, σ3=15. Objetivo: 47200"         # I8
+# "I10\nσ1=10, σ2=5, σ3=15. Objetivo: 98400"        # I10
+# "I11\nσ1=10, σ2=5, σ3=15. Objetivo: 139500"       # I11
 # "I12\nσ1=10, σ2=5, σ3=15. Objetivo: 21807.5"      # I12
+# "I13\nσ1=10, σ2=5, σ3=15. Objetivo: 20100"        # I13
+# "I14\nσ1=10, σ2=5, σ3=15. Objetivo: 15750"        # I14
 
-output = "plots/timetotarget_10-5-15_i11_N50_t139500_Indefinidas - i11"
-# "timetotarget_10-5-15_i1_N100_t260870_randomFit_r2_s15_t8-4"               # I1
-# "timetotarget_10-5-15_i2_N100_t427778_randomFit_r2_s15_t16-4"              # I2
-# "timetotarget_10-5-15_i3_N100_t383300_randomFit_r2_s50_t8-4"               # I3
-# "timetotarget_10-5-15_i4_N100_t1497200_randomFit_r2_s50_t16-4"             # I4
-# "timetotarget_10-5-15_i5_N50_t1526900_randomFit_r5_s100_t8-4"              # I5
-#
-# "timetotarget_10-5-15_i12_N50_t21807_fullrand_s70_p1-4_w20_t5-20_e5_g20"    # I12
+output = "plots/timetotarget_10-5-15_i10_N50_t98400_Indefinidas - i10"
+# "plots/timetotarget_10-5-15_i1_N100_t260870_randomFit_r2_s15_t8-4"                # I1
+# "plots/timetotarget_10-5-15_i2_N100_t427778_randomFit_r2_s15_t16-4"               # I2
+# "plots/timetotarget_10-5-15_i3_N100_t383300_randomFit_r2_s50_t8-4"                # I3
+# "plots/timetotarget_10-5-15_i4_N100_t1497200_randomFit_r2_s50_t16-4"              # I4
+# "plots/timetotarget_10-5-15_i5_N50_t1526900_randomFit_r5_s100_t8-4"               # I5
+# "plots/timetotarget_10-5-15_i8_N50_t47200_Indefinidas - i8"                       # I8
+# "plots/timetotarget_10-5-15_i10_N50_t98400_Indefinidas - i10"                     # I10
+# "plots/timetotarget_10-5-15_i11_N50_t139500_Indefinidas - i11"                    # I11
+# "plots/timetotarget_10-5-15_i12_N50_t21807_fullrand_s70_p1-4_w20_t5-20_e5_g20"    # I12
+# "plots/timetotarget_10-5-15_i13_N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15"    # I13
+# "plots/timetotarget_10-5-15_i14_N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20"    # I14
 
 files = [
-    ("ttt_allOps_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
-    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso, Pior e Shaw", 1000),
-    ("ttt_rand+greedy+worst_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso e Pior", 1000),
-    ("ttt_rand+greedy_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório e Guloso", 1000),
-    ("ttt_randOnly_10-5-15__N50_t139500_Indefinidas - i11.dat", "Somente aleatório", 1000),
+    ("ttt_allOps_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
+    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso, Pior e Shaw", 1000),
+    ("ttt_rand+greedy+worst_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso e Pior", 1000),
+    ("ttt_rand+greedy_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório e Guloso", 1000),
+    ("ttt_randOnly_10-5-15__N50_t98400_Indefinidas - i10.dat", "Somente aleatório", 1000),
 ]
 
 # I1
@@ -72,6 +80,33 @@ files = [
 #    ("ttt_randOnly_10-5-15__N50_t1526900_randomFit_r5_s100_t8-4.dat", "Somente aleatório", 1000),
 #]
 
+# I8
+#[
+#    ("ttt_allOps_10-5-15__N50_t47200_Indefinidas - i8.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
+#    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t47200_Indefinidas - i8.dat", "Aleatório, Guloso, Pior e Shaw", 1000),
+#    ("ttt_rand+greedy+worst_10-5-15__N50_t47200_Indefinidas - i8.dat", "Aleatório, Guloso e Pior", 1000),
+#    ("ttt_rand+greedy_10-5-15__N50_t47200_Indefinidas - i8.dat", "Aleatório e Guloso", 1000),
+#    ("ttt_randOnly_10-5-15__N50_t47200_Indefinidas - i8.dat", "Somente aleatório", 1000),
+#]
+
+# I10
+#[
+#    ("ttt_allOps_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
+#    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso, Pior e Shaw", 1000),
+#    ("ttt_rand+greedy+worst_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório, Guloso e Pior", 1000),
+#    ("ttt_rand+greedy_10-5-15__N50_t98400_Indefinidas - i10.dat", "Aleatório e Guloso", 1000),
+#    ("ttt_randOnly_10-5-15__N50_t98400_Indefinidas - i10.dat", "Somente aleatório", 1000),
+#]
+
+# I11
+#[
+#    ("ttt_allOps_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
+#    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso, Pior e Shaw", 1000),
+#    ("ttt_rand+greedy+worst_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório, Guloso e Pior", 1000),
+#    ("ttt_rand+greedy_10-5-15__N50_t139500_Indefinidas - i11.dat", "Aleatório e Guloso", 1000),
+#    ("ttt_randOnly_10-5-15__N50_t139500_Indefinidas - i11.dat", "Somente aleatório", 1000),
+#]
+
 # I12
 #[
 #    ("ttt_allOps_10-5-15__N50_t21807_fullrand_s70_p1-4_w20_t5-20_e5_g20.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 1000),
@@ -81,11 +116,32 @@ files = [
 #    ("ttt_randOnly_10-5-15__N50_t21807_fullrand_s70_p1-4_w20_t5-20_e5_g20.dat", "Somente aleatório", 1000),
 #]
 
-teto = 200
+# I13
+#[
+#    ("ttt_allOps_10-5-15__N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15.dat", "Aleatório, Guloso, Pior,\nShaw e Arrependimento", 1000),
+#    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15.dat", "Aleatório, Guloso, Pior\ne Shaw", 1000),
+#    ("ttt_rand+greedy+worst_10-5-15__N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15.dat", "Aleatório, Guloso e Pior", 1000),
+#    ("ttt_rand+greedy_10-5-15__N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15.dat", "Aleatório e Guloso", 1000),
+#    ("ttt_randOnly_10-5-15__N50_t20100_fullrand_s70_p1-4_w30_t5-20_e3_g15.dat", "Somente aleatório", 1000),
+#]
+
+# I14
+#[
+#    ("ttt_allOps_10-5-15__N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20.dat", "Aleatório, Guloso, Pior, Shaw e Arrependimento", 500),
+#    ("ttt_rand+greedy+worst+shaw_10-5-15__N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20.dat", "Aleatório, Guloso, Pior e Shaw", 500),
+#    ("ttt_rand+greedy+worst_10-5-15__N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20.dat", "Aleatório, Guloso e Pior", 500),
+#    ("ttt_rand+greedy_10-5-15__N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20.dat", "Aleatório e Guloso", 500),
+#    ("ttt_randOnly_10-5-15__N50_t15750_fullrand_s90_p1-4_w20_t6-20_e5_g20.dat", "Somente aleatório", 500),
+#]
+
+
+teto = 50
 # 8      # I1
 # 6      # I2
 # 50     # I3
 # 500    # I4
+# 50     # I10
+# 100    # I13
 # 500    # I12
 
 fig = plt.Figure()
@@ -113,7 +169,7 @@ plt.xlabel("Tempo (s)")
 plt.ylabel("Probabilidade Acumulada")
 plt.title(titulo)
 plt.grid(color='grey', linestyle='-', linewidth=1, alpha=.1)
-plt.legend()
+plt.legend(loc="lower right")
 
 plt.savefig(f"{output}.pdf")
 plt.show()
